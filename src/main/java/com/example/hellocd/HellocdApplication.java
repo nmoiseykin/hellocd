eccc37ba4ddc1f8e373304c965dbb00f322580f5
N.Moiseykin <moiseykin@gmail.com>
2021-12-22 05:12:19 +0000

    changed hello world text

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 		@RequestMapping("/")
 			String home() {
-						return "Hello World!";
+						return "Hello Continious Delivery!";
 							}
 
 			public static void main(String[] args) {
